da6f65d697cf7eaff428286fb93ff552d78f231f
Ming Duan <11203duan@gmail.com>
2019-03-26 00:24:19 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,7 @@ class Person
 attr_accessor
 
 def initialize(attributes)
-  attributes.each { |key, value| self.send("key=", value)}
+  attributes.each { |key, value| self.send(("#{key}="), value)}
 end
 
 
